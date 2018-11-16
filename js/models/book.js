aba6d60690d8ebaa99aca017401c8202fa18fb98
Tucker <trblackw@ncsu.edu>
2018-11-15 19:45:10 -0500

    changed defaults

--- js/models/book.js
+++ js/models/book.js
@@ -3,12 +3,12 @@ var app = app || {};
 app.Book = Backbone.Model.extend({
   defaults: {
     imageLinks: "",
-    title: "No title",
-    author: "Unknown",
-    publishedDate: "Unknown",
-    keywords: "None" || [],
-    publisher: "publisher",
-    description: "description",
+    title: "",
+    author: "",
+    publishedDate: "",
+    keywords: [],
+    publisher: "",
+    description: "",
     categories: []
   }
 });
